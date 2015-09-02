622fb702d8d1b821fc788e6170a02c4df4534e26
Sam Van Oort <samvanoort@gmail.com>
2015-09-02 11:56:35 -0400

    Dockerfile for gerrit based on gerrit base dockerfile, from gerritt installer

--- runs.sh
+++ runs.sh
@@ -1,3 +1,6 @@
 # Gerrit demo
-docker run -d -p 8080:8080 -p 29418:29418 \
-  gerritforge/gerrit-ubuntu15.04
+#docker run -d -p 8080:8080 -p 29418:29418 \
+#  gerritforge/gerrit-ubuntu15.04
+
+docker build -t jenkins-gerrit-wf-demo:1.0 .
+docker run -d -p 8080:8080 -p 29418:29418 jenkins-gerrit-wf-demo:1.0
