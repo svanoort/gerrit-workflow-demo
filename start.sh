063136be1d0f17248c21e4ba7f5f45e5f4c988d8
Sam Van Oort <samvanoort@gmail.com>
2015-09-08 10:16:00 -0400

    Gerrit setup to create repos and start configuring for repo, functional state of git server indeterminate

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 # Starts both containers in  daemon mode
 docker run -d -p 8081:8080 jenkins-gerrit-wfdemo-jenkins:1.0
-docker run -d -p 8080:8080 -p 29418:29418 jenkins-gerrit-wfdemo-gerrit:1.0
+docker run -d -p 8080:8080 -p 29418:29418 -p 9418:9418 jenkins-gerrit-wfdemo-gerrit:1.0
