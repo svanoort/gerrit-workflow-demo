0dd8feca0e4851a540d5b92b84086f32a8c9729e
Sam Van Oort <samvanoort@gmail.com>
2015-09-10 15:21:27 -0400

    Remove git daemon setup since that's through gerrit now

--- gerrit/run.sh
+++
@@ -1,3 +0,0 @@
-# Expose git servers and start up gerrit
-git daemon --verbose --enable=receive-pack --base-path=/tmp/repos --export-all &
-/var/gerrit/bin/gerrit.sh start && tail -f /var/gerrit/logs/error_log
