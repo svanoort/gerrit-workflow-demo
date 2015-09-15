3bc0a1f7c929691d909ee7823ec1271f53cbd276
Sam Van Oort <samvanoort@gmail.com>
2015-09-14 22:07:57 -0400

    Fix up volume testing

--- testme.sh
+++ testme.sh
@@ -1,4 +1,4 @@
 # Debugging use, runs removable containers in interactive mode if needed
 
-sudo docker run  -i -t --rm=true -p 8081:8080 jenkins-gerrit-wfdemo-jenkins:1.0
-sudo docker run  -i -t --rm=true -p 8080:8080 -p 29418:29418 jenkins-gerrit-wfdemo-gerrit:1.0 /bin/bash
+sudo docker run  -i -t --rm=true --name gerrit -p 8080:8080 -p 29418:29418 jenkins-gerrit-wfdemo-gerrit:1.0 /bin/bash
+sudo docker run  -i -t --rm=true -v ./jenkins/jenkins_home:/var/jenkins_home -p 8081:8080 --link gerrit:gerrit jenkins-gerrit-wfdemo-jenkins:1.0 /bin/bash
