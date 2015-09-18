41c7f73309ed0ce32ec738ed0ce9e5e7888291f7
Sam Van Oort <samvanoort@gmail.com>
2015-09-18 13:44:11 -0400

    Starting point for configuring your local git user with gerrit

--- config-gerrit.sh
+++ config-gerrit.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+USERNAME="`git config user.name`"
+EMAIL="`git config user.email`"
+USERNAME="$USER"
+HTTP_PASS=goober
+FULLNAME="`git config user.name`"
 
+cat ~/.ssh/id_rsa.pub | ssh -i jenkins/demo_key_rsa -p 29418 demouser@localhost \
+  gerrit create-account --group Workers --full-name \"$FULLNAME\" --email \"$EMAIL\" --http-password \"$HTTP_PASS\" --ssh-key - $USERNAME
