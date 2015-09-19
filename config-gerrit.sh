f7c53de4ed637c329b3e1926062b40d4899631b3
Sam Van Oort <samvanoort@gmail.com>
2015-09-18 22:46:04 -0400

    Config git user script adds user to non-interactive users

--- config-gerrit.sh
+++ config-gerrit.sh
@@ -6,4 +6,4 @@ HTTP_PASS=goober
 FULLNAME="`git config user.name`"
 
 cat ~/.ssh/id_rsa.pub | ssh -i jenkins/demo_key_rsa -p 29418 demouser@localhost \
-  gerrit create-account --group Workers --full-name \"$FULLNAME\" --email \"$EMAIL\" --http-password \"$HTTP_PASS\" --ssh-key - $USERNAME
+  gerrit create-account --group Workers --group \"Non-Interactive Users\" --full-name \"$FULLNAME\" --email \"$EMAIL\" --http-password \"$HTTP_PASS\" --ssh-key - $USERNAME
