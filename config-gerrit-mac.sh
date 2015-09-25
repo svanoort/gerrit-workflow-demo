c112b21862e5d85e9d98209f6f0772183d207d19
Sam Van Oort <samvanoort@gmail.com>
2015-09-25 17:26:48 -0400

    Fix permissions issue with config scripts

--- config-gerrit-mac.sh
+++ config-gerrit-mac.sh
@@ -8,6 +8,6 @@ FULLNAME="`git config user.name`"
 # Fixes issues with repo url getting wrong username, since it uses local part of
 # email for SSH username
 git config --global review.http://gerrit:8080/.username $USER
-
+chmod 400 jenkins/demo_key_rsa
 cat ~/.ssh/id_rsa.pub | ssh -i jenkins/demo_key_rsa -p 29418 demouser@gerrit \
       gerrit create-account --group Workers --group \"Non-Interactive Users\" --full-name \"$FULLNAME\" --email \"$EMAIL\" --http-password \"$HTTP_PASS\" --ssh-key - $USERNAME
